3ed0f0fb5df18e4cdac618203c0c03c757a52e9e
VysyarajuHansuja <vhansuja2006@gmail.com>
2026-05-15 23:35:40 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,22 +1,13 @@
 import sqlite3
-import os
-
-
-BASE_DIR = os.path.dirname(
-    os.path.abspath(__file__)
-)
-
-DB_PATH = os.path.join(
-    BASE_DIR,
-    "task_manager.db"
-)
 
 
 def connect_db():
 
     conn = sqlite3.connect(
-        DB_PATH,
+        "task_manager.db",
         check_same_thread=False
     )
+
+    conn.row_factory = sqlite3.Row
 
     return conn
